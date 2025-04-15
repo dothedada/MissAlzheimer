395e617d2f0fb58068134c40ef461296c305a45c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-04-15 10:22:32 -0500

    feat(card): Removes id, hyphens and trailling spaces from alt texts

--- src/app/__test__/card.test.ts
+++ src/app/__test__/card.test.ts
@@ -1,18 +1,46 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { Card } from '../card';
 import fetchDataMock from '../hooks/__test__/fetchMock.json';
 
 describe('Card', () => {
     const imgsData = fetchDataMock as FetchData[];
+    let i: number;
+    let j: number;
+
+    beforeEach(() => {
+        ((max: number): [number, number] => {
+            i = Math.floor(Math.random() * max);
+            j = Math.floor(Math.random() * (max - 1));
+
+            if (j >= i) {
+                j++;
+            }
+            return [i, j];
+        })(imgsData.length);
+    });
+
     it('should create a card instance with the given data', () => {
         for (const imgData of imgsData) {
             const card = new Card(imgData);
             expect(card).toBeInstanceOf(Card);
             expect(card.author).toBe(imgData.user.name);
-            expect(card.altEs).toBe(imgData.alternative_slugs.es);
-            expect(card.altEn).toBe(imgData.slug);
             expect(card.img.src).toBe(`${imgData.urls.small}`);
             expect(card.id).toBe(imgData.id);
         }
+    });
+
+    it('sould removes the id from the alt texts and replace hyphens', () => {
+        const newCard1 = new Card(imgsData[i]);
+        const newCard2 = new Card(imgsData[j]);
+
+        expect(newCard1.altEn).not.toContain(newCard1.id);
+        expect(newCard2.altEn).not.toContain(newCard2.id);
+        expect(newCard1.altEs).not.toContain(newCard1.id);
+        expect(newCard2.altEs).not.toContain(newCard2.id);
+        expect(newCard1.altEn).not.toContain('-');
+        expect(newCard2.altEn).not.toContain('-');
+        expect(newCard1.altEs).not.toContain('-');
+        expect(newCard2.altEs).not.toContain('-');
+        console.log(newCard1);
     });
 });
--- src/app/card.ts
+++ src/app/card.ts
@@ -11,12 +11,21 @@ export class Card {
     constructor(data: FetchData) {
         this.id = data.id;
         this.author = data.user.name;
-        this.altEs = data.alternative_slugs.es;
-        this.altEn = data.slug;
+        this.altEs = this.cleantAltTxt(data.alternative_slugs.es);
+        this.altEn = this.cleantAltTxt(data.slug);
 
         const img = new Image();
         img.src = data.urls.small;
         img.alt = this.altEn;
         this.img = img;
+    }
+
+    /**
+     * Removes the ID from the slug and changes the hyphens to ' '
+     * @param txt takes the string to clean up
+     * @returns a string with no ID, hyphens or trailling whitespace
+     */
+    cleantAltTxt(txt: string): string {
+        return txt.replace(this.id, '').replace(/-/g, ' ').trim();
     }
 }
